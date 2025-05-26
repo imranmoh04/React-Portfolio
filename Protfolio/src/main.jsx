8cf6fba401744a9f6eb8c86588cb1c7221f2fbae
Mohid Imran <imranmoh@msu.edu>
2025-05-26 11:40:09 -0400

    Navbar creation

--- Protfolio/src/main.jsx
+++ Protfolio/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import "@fontsource-variable/manrope"
+import "@fontsource-variable/space-grotesk"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
